42fa120f08f50cdbafead570cc4db8ee7a22f42d
haku90 <adam.hakowski@gmail.com>
2016-02-28 20:10:14 +0100

    Added UnitTest

---
+++ UnitTestForWFProgram/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Activities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestForWFProgram
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            const string correctOutputValue = "Test Message";
+            var output = WorkflowInvoker.Invoke(new WorkflowForTest());
+            Assert.AreEqual(correctOutputValue, output["OutMessage"]);
+        }
+    }
+}
