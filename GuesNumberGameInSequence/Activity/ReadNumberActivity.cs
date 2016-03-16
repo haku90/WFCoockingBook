59f0cbf08868f060609e68bc5b6424b670046330
haku90 <adam.hakowski@gmail.com>
2016-03-16 19:52:31 +0100

    Added GuesNumberGame

---
+++ GuesNumberGameInSequence/Activity/ReadNumberActivity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Activities;
+
+namespace GuesNumberGameInSequence.Activity
+{
+    public class ReadNumberActivity : CodeActivity
+    {
+        public OutArgument<int> OutNumber { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            OutNumber.Set(context, Int32.Parse(Console.ReadLine()));
+        }
+    }
+}
---
+++ GuesNumberGameInSequence/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Activities;
+
+namespace GuesNumberGameInSequence
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workflow1 = new Workflow1();
+            WorkflowInvoker.Invoke(workflow1);
+        }
+    }
+}
