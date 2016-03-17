b5b11dde6eba5f4ab9407c6c1381dd9a6d5f78b6
haku90 <adam.hakowski@gmail.com>
2016-03-17 17:53:34 +0100

    Finished GuessNumberInFlowChart

---
+++ GuessNumberInFlowChart/ReadNumberActivity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Activities;
+
+namespace GuessNumberInFlowChart
+{
+    public class ReadNumberActivity : CodeActivity
+    {
+        public OutArgument<int> OutNumber { get; set; }
+        
+        protected override void Execute(CodeActivityContext context)
+        {
+            OutNumber.Set(context, Int32.Parse(Console.ReadLine()));
+        }
+    }
+}
