11720f47c204202f0bed2644519fd89fedb3f1e2
haku90 <adam.hakowski@gmail.com>
2016-03-19 15:02:25 +0100

    added UsingSwitchActivityInSequenceWorkflow

---
+++ UsingSwitchActivityInSequenceWorkflow/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace UsingSwitchActivityInSequenceWorkflow
+{
+    [TypeConverter(typeof(ProductConverter))]
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public Guid ProductId { get; set; }
+
+        public Product()
+        {
+            ProductName = "Defualt name";
+            ProductId = Guid.NewGuid();
+        }
+
+        public Product(string productName, Guid productId)
+        {
+            ProductName = productName;
+            ProductId = productId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var product = obj as Product;
+            return product != null && String.Equals(ProductId, product.ProductId);
+        }
+
+        public override int GetHashCode()
+        {
+            return ProductName != null ? ProductName.GetHashCode() : 0;
+        }
+    }
+}
---
+++ UsingSwitchActivityInSequenceWorkflow/ProductConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace UsingSwitchActivityInSequenceWorkflow
+{
+    public class ProductConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof (string);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value == null)
+                return null;
+            if (value is string)
+            {
+                return new Product
+                {
+                    ProductName = (string) value,
+                    ProductId = Guid.NewGuid()
+                };
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof (string))
+            {
+                return value != null ? ((Product) value).ProductName : null;
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
