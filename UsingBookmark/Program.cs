75751904c34bcd4c76390433dd36e3d59ce1ce6d
haku90 <adam.hakowski@gmail.com>
2016-03-21 21:20:12 +0100

    Added UsingBookmark

---
+++ UsingBookmark/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+using System.Threading;
+
+namespace UsingBookmark
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var waitHandler = new AutoResetEvent(false);
+            var bookmarkName = "inputBookmark";
+            var wfApp = new WorkflowApplication(new TestInputMessageWF()
+            {
+                bookmarkName = bookmarkName
+            });
+
+            wfApp.Completed = (arg) => { waitHandler.Set(); };
+            wfApp.Run();
+            wfApp.ResumeBookmark(bookmarkName, Console.ReadLine());
+            waitHandler.WaitOne();
+            
+        }
+    }
+}
