96e545546d8df83c7ee190d0c319fccfe0c59f5f
haku90 <adam.hakowski@gmail.com>
2016-04-03 16:51:46 +0200

    Added CreateInputMessageActivityUsingBookmark

---
+++ CreateInputMessageActivityUsingBookmark/InputMessageActivity.cs
@@ -0,0 +1,22 @@
+﻿using System.Activities;
+
+namespace CreateInputMessageActivityUsingBookmark
+{
+    public class InputMessageActivity<T> : NativeActivity
+    {
+        public InArgument<string> BookmarkName { get; set; }
+        public OutArgument<T> Result { get; set; }
+
+        protected override void Execute(NativeActivityContext context)
+        {
+            context.CreateBookmark(BookmarkName.Get(context), new BookmarkCallback(OnResumeBookmark));
+        }
+
+        public void OnResumeBookmark(NativeActivityContext context, Bookmark bookmark, object value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool CanInduceIdle => true;
+    }
+}
