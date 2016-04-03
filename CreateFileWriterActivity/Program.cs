335f5e52353cd1d74356b73a73edbf43e0011c57
haku90 <adam.hakowski@gmail.com>
2016-04-03 12:43:10 +0200

    added wf code style

--- CreateFileWriterActivity/Program.cs
+++ CreateFileWriterActivity/Program.cs
@@ -10,7 +10,26 @@ namespace CreateFileWriterActivity
     {
         static void Main(string[] args)
         {
-            WorkflowInvoker.Invoke(new TestFileWriterWF());
+            //WorkflowInvoker.Invoke(new TestFileWriterWF());
+            WorkflowInvoker.Invoke(CodeStyleWorkFlow());
+        }
+
+        private static Activity CodeStyleWorkFlow()
+        {
+            var wf = new Sequence
+            {
+                Activities =
+                {
+                    new WriteLine {Text = "Start..."},
+                    new FileWriterActivity
+                    {
+                        FileName = "Test.txt",
+                        FileData = "Text content"
+                    },
+                    new WriteLine {Text = "End..."}
+                }
+            };
+            return wf;
         }
     }
 }
