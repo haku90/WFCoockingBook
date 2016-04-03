e5915af20749d32af4de8a1478db60dcff6a5ffa
haku90 <adam.hakowski@gmail.com>
2016-04-03 12:37:20 +0200

    Added FileWriter

---
+++ CreateFileWriterActivity/FileWriterActivity.cs
@@ -0,0 +1,26 @@
+﻿using System.Activities;
+using System.IO;
+using System.Threading;
+
+namespace CreateFileWriterActivity
+{
+    public class FileWriterActivity : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<string> FileName { get; set; }
+
+        [RequiredArgument]
+        public InArgument<string> FileData { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var lines = FileData.Get(context);
+            using (var file = new StreamWriter(FileName.Get(context)))
+            {
+                file.WriteLine(lines);
+                Thread.Sleep(5000);
+                file.Close();
+            }
+        }
+    }
+}
