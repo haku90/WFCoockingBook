064ad4b6753c9ee8977bdac7dde429c62d6a54c4
haku90 <adam.hakowski@gmail.com>
2016-04-06 20:26:17 +0200

    Added CreateMySequenceActivity

---
+++ CreateMySequenceActivity/MySequence .cs
@@ -0,0 +1,42 @@
+﻿using System.Activities;
+using System.Collections.ObjectModel;
+
+namespace CreateMySequenceActivity
+{
+    public class MySequenceActivity : NativeActivity
+    {
+        public Collection<Activity> Activities { get; set; }
+        private int _activityCounter;
+
+        public MySequenceActivity()
+        {
+            Activities = new Collection<Activity>();
+        }
+
+        protected override void CacheMetadata(NativeActivityMetadata metadata)
+        {
+            foreach (var activity in Activities)
+            {
+                metadata.AddImplementationChild(activity);
+            }
+        }
+
+        protected override void Execute(NativeActivityContext context)
+        {
+            ScheduleActivities(context);
+        }
+
+        private void ScheduleActivities(NativeActivityContext context)
+        {
+            if (_activityCounter < Activities.Count)
+            {
+                context.ScheduleActivity(Activities[_activityCounter++], OnActivityCompleted);
+            }
+        }
+
+        private void OnActivityCompleted(NativeActivityContext context, ActivityInstance completedinstance)
+        {
+            ScheduleActivities(context);
+        }
+    }
+}
---
+++ CreateMySequenceActivity/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+
+namespace CreateMySequenceActivity
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkflowInvoker.Invoke(GetTestMySequenceWF());
+        }
+
+        static Activity GetTestMySequenceWF()
+        {
+            return new MySequenceActivity
+            {
+                Activities =
+                {
+                    new WriteLine { Text = "WriteLine1" },
+                    new WriteLine { Text = "WriteLine2" },
+                    new WriteLine {Text = "WriteLine3"}
+                }
+            };
+        }
+    }
+}
