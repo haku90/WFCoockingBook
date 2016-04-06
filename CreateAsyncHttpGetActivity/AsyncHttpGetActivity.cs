06da304489fcc69d14e8c4e161ff343ebe3f2e29
haku90 <adam.hakowski@gmail.com>
2016-04-06 19:47:05 +0200

    Added CreateAsyncHttpGetActivity

---
+++ CreateAsyncHttpGetActivity/AsyncHttpGetActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Activities;
+using System.IO;
+using System.Net;
+
+namespace CreateAsyncHttpGetActivity
+{
+    public class AsyncHttpGetActivity : AsyncCodeActivity<string>
+    {
+        public InArgument<string> Uri { get; set; }
+
+        protected override IAsyncResult BeginExecute(AsyncCodeActivityContext context, AsyncCallback callback, object state)
+        {
+            var request = HttpWebRequest.Create(Uri.Get(context));
+            context.UserState = request;
+            return request.BeginGetResponse(callback, state);
+        }
+
+        protected override string EndExecute(AsyncCodeActivityContext context, IAsyncResult result)
+        {
+            var request = context.UserState as WebRequest;
+            using (var response = request.EndGetResponse(result))
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
