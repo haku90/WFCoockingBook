86ccdf7cd2684b79d04a2599c304d3f35dd2ec49
haku90 <adam.hakowski@gmail.com>
2016-04-10 12:53:29 +0200

    Added TestSendEmailWF to  CreateActivityDesignerForSendEmail

---
+++ CreateActivityDesignerForSendEmail/SendEmailActivity.cs
@@ -0,0 +1,36 @@
+﻿using System.Activities;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Mail;
+
+namespace CreateActivityDesignerForSendEmail
+{
+    [Designer(typeof(SendEmailActivityDesigner))]
+    public class SendEmailActivity : CodeActivity
+    {
+        public InArgument<string> From { get; set; }
+        public InArgument<string> Host { get; set; }
+        public InArgument<string> UserName { get; set; }
+        public InArgument<string> Password { get; set; }
+        public InArgument<string> To { get; set; }
+        public InArgument<string> Subject { get; set; }
+        public InArgument<string> Body { get; set; }
+        public OutArgument<string> Result { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var mailMassage = new MailMessage();
+            mailMassage.To.Add(To.Get(context));
+            mailMassage.Subject = Subject.Get(context);
+            mailMassage.Body = Body.Get(context);
+            mailMassage.From = new MailAddress(From.Get(context));
+
+            var smtp = new SmtpClient();
+            smtp.Host = Host.Get(context);
+            smtp.Credentials = new NetworkCredential(UserName.Get(context), Password.Get(context));
+            smtp.EnableSsl = true;
+            smtp.Send(mailMassage);
+            Result.Set(context, "Send Email successfully!");
+        }
+    }
+}
--- CreateActivityDesignerForSendEmail/SendEmailActivityDesigner.xaml.cs
+++ CreateActivityDesignerForSendEmail/SendEmailActivityDesigner.xaml.cs
@@ -19,7 +19,7 @@ namespace CreateActivityDesignerForSendEmail
     {
         public SendEmailActivityDesigner()
         {
-            InitializeComponent();
+            //InitializeComponent();
         }
     }
 }
