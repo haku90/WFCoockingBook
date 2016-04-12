08c8716533d033a604c91189b8f4ff542b771eaa
haku90 <adam.hakowski@gmail.com>
2016-04-12 11:59:10 +0200

    Finished LoadUpWorkflowFromPersistenceDb and Added UsingPersistenceParticipant

---
+++ LoadUpWorkflowFromPersistenceDb/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.DurableInstancing;
+using System.Activities.Statements;
+using System.Threading;
+
+namespace LoadUpWorkflowFromPersistenceDb
+{
+
+    class Program
+    {
+        private static SqlWorkflowInstanceStore _sqlWorkflowInstanceStore = SetUpSqlPersistenceStore();
+
+        static void Main(string[] args)
+        {
+            StartAndUnloadInstance();
+        }
+
+        private static void StartAndUnloadInstance()
+        {
+            var waitHandler = new AutoResetEvent(initialState: false);
+            var wfApp = new WorkflowApplication(new Workflow1());
+            wfApp.InstanceStore = _sqlWorkflowInstanceStore;
+            wfApp.PersistableIdle = (e) =>
+            {
+                return PersistableIdleAction.Unload;
+                
+            };
+            wfApp.Unloaded = (e) => { waitHandler.Set(); };
+            var id = wfApp.Id;
+            wfApp.Run();
+            waitHandler.WaitOne();
+            LoadAndCompleteInstance(id);
+        }
+
+        private static void LoadAndCompleteInstance(Guid id)
+        {
+            Console.WriteLine("Press <enter> to load the persisted workflow");
+            Console.ReadLine();
+            var waitHandler = new AutoResetEvent(initialState: false);
+            var wfApp = new WorkflowApplication(new Workflow1());
+            wfApp.InstanceStore = _sqlWorkflowInstanceStore;
+            wfApp.Unloaded = (workflowApplicationEventArgs) => { waitHandler.Set(); };
+            wfApp.Load(id);
+            wfApp.Run();
+            waitHandler.WaitOne();
+        }
+
+        private static SqlWorkflowInstanceStore SetUpSqlPersistenceStore()
+        {
+            var sqlPersistenceDbConnectionString = @"Server=.\SQLEXPRESS;Initial Catalog=PersistenceDatabase;Integrated Security=True";
+
+            var sqlWFInstanceStore = new SqlWorkflowInstanceStore(sqlPersistenceDbConnectionString);
+            return sqlWFInstanceStore;
+        }
+
+    }
+}
