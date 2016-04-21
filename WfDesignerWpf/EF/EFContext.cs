8a3eec4326aff70db0b25f032d734db64e215ba2
haku90 <adam.hakowski@gmail.com>
2016-04-21 11:33:46 +0200

    Added EF 7

---
+++ WfDesignerWpf/EF/EFContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.Entity;
+using WfDesignerWpf.Models;
+
+namespace WfDesignerWpf.EF
+{
+    public class EFContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=WFDesigner;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<DocumentPropertiesInfo>();
+            modelBuilder.Entity<PropertyInfo>();
+        }
+    }
+}
